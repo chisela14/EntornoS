bab576a06bf41cebdfef1abba0aa5021b53824a7
chisela14 <chisela14@gmail.com>
2022-12-12 10:08:25 +0100

    inicio calculadora

---
+++ springCalculator/src/main/java/com/jacaranda/calculator/model/Calculator.java
@@ -0,0 +1,16 @@
+package com.jacaranda.calculator.model;
+
+public class Calculator {
+	
+	private float num1;
+	private float num2;
+	
+	public Calculator(float num1, float num2) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	
+
+}
