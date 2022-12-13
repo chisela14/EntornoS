d036402e9712f56c5fbfa423fd1f99825c18ed94
chisela14 <chisela14@gmail.com>
2022-12-13 10:06:55 +0100

    spring crud

---
+++ springCrud/src/test/java/com/jacaranda/crud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacaranda.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
